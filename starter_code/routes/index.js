eb726aa98ae23cf54bd88194e4bfe7681024ed13
Samuraty <alexandra.garciagasco@gmail.com>
2018-07-13 13:22:20 +0200

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
 const User = require("../models/User");
+const LaundryPickup = require("../models/Laundry-pickup");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
-router.get("/dashboard",ensureLogin.ensureLoggedIn(), (req, res, next) => {
-  res.render("laundry/dashboard");
-});
+
 router.post("/launderers",ensureLogin.ensureLoggedIn(), (req, res, next) => {
   const { fee } = req.body;
   User.findByIdAndUpdate(req.user._id, { isLaunderer: true, fee }).then(
@@ -36,6 +35,62 @@ router.get("/launderers", ensureLogin.ensureLoggedIn(), (req, res, next) => {
       console.log("Error getting launderers");
     })
 })
+
+router.get("/launderers/:id", (req, res, next) => {
+  User.findById(req.params.id)
+    .then(theLaunderer => {
+      res.render("laundry/launderer-profile", {theLaunderer})
+    })
+    .catch(() => {
+      console.log("Error showing launderer");
+    })
+});
+
+router.post('/laundry-pickups', (req, res, next) => {
+  console.log(req.user)
+  const pickupInfo = {
+    pickupDate: req.body.pickupDate,
+    launderer: req.body.laundererId,
+    user: req.user._id
+  };
+  const thePickup = new LaundryPickup(pickupInfo);
+  thePickup.save((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.redirect('/dashboard');
+  });
+});
+
+router.get('/dashboard', (req, res, next) => {
+  let query;
+
+  if (req.user.isLaunderer) {
+    query = { launderer: req.user._id };
+  } else {
+    query = { user: req.user._id };
+  }
+
+  LaundryPickup
+    .find(query)
+    .populate('user', 'name')
+    .populate('launderer', 'name')
+    .sort('pickupDate')
+    .exec((err, pickupDocs) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      res.render('laundry/dashboard', {
+        pickups: pickupDocs
+      });
+    });
+});
+
+
+
 
 
 module.exports = router;
